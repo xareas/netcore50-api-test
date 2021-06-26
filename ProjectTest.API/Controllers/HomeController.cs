81146d12f32c97d31c0d5fd03e49a5a9727345bc
Stywar <61365253+Stywar@users.noreply.github.com>
2021-06-26 18:34:14 -0500

    Update HomeController.cs

--- ProjectTest.API/Controllers/HomeController.cs
+++ ProjectTest.API/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ProjectTest.API.Controllers
         public string Get()
         {
             _logger.LogInformation("Initial Home Get");
-            return "Net Core 5.0 Running ... 26062021-v2.1.12 Jenkins Pipeline AFORO255";
+            return "Net Core 5.0 Running ... 26062021-v2.1.12 Jenkins Pipelines AFORO255";
         }
     }
 }
