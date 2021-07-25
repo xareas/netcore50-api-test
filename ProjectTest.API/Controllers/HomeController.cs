1fbace19adf8e8d3dbf046381cd9915c079ee301
Stywar <franco111988@gmail.com>
2021-07-24 20:53:33 -0500

    change file

--- ProjectTest.API/Controllers/HomeController.cs
+++ ProjectTest.API/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ProjectTest.API.Controllers
         public string Get()
         {
             _logger.LogInformation("Initial Home Get");
-            return "Net Core 5.0 Running ... 26062021-v2.1.12 Jenkins Pipelines AFORO255";
+            return "Net Core 5.0 Running ... 24072021-v1.0.0 Azure DevOps AFORO255";
         }
     }
 }
