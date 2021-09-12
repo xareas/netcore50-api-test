1268c206ac22b23aa0022cae489392ac70b0b9f2
Stywar <61365253+Stywar@users.noreply.github.com>
2021-09-11 19:57:10 -0500

    Update HomeController.cs

--- ProjectTest.API/Controllers/HomeController.cs
+++ ProjectTest.API/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ProjectTest.API.Controllers
         public string Get()
         {
             _logger.LogInformation("Initial Home Get");
-            return "Net Core 5.0 Running ... 08072021-v1.0.0 JENKINS AFORO255";
+            return "Net Core 5.0 Running ... 110092021-v1.0.0 AZURE DEVOPS AFORO255";
         }
     }
 }
